5e5a57c498e814083e7726ff5cf9a5b4ee6e1f01
Sebastian Marin <sebastian.marin.val@gmail.com>
2022-05-31 15:21:48 -0500

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '680287896353af128a4741b1f2fe3596'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3bc9815-648a-4c21-8da3-5813340e370c'
+};
